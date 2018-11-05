fad533815c17d23547fd77b7d380011b3dd6834e
Sean Langton <slangton1@gmail.com>
2018-11-05 02:41:53 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ def initialize(roster)
   @roster = []
 end
 
-#def add_student(name, grade)
-#  @roster[grade] = []
-#  @roster[grade] << name
-#end
+def add_student(name, grade)
+  @roster[grade] = []
+  @roster[grade] << name
+end
